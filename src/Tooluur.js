939a033d6eb0e1e99835bf43502347762396f1cf
temk202 <temuujin2426@gmail.com>
2022-10-22 14:20:13 +0800

    button hiiv

--- src/Tooluur.js
+++ src/Tooluur.js
@@ -1,29 +1,50 @@
+import './Tooluur.css';
 import './App.css';
 import { useState } from "react";
 import Button from '@mui/material/Button';
 
 
 function App() {
-  const [number, setNumber] =useState(0)
+  const [input, setInput] = useState("")
+  const [names, setNames] = useState([])
 
-  const changeNumber = (temka) => {
-    console.log(temka)
+  const addNames =()=>{
+   var array = [...names]
+   array.push(input)
+   setNames(array)
 
-    if(temka === "nemeh") {
-        setNumber(number+1)
-    } else if(temka === "hasah") {
-        setNumber(number - 1)
-    } else if(temka === "urjih") {
-        setNumber(number * 2)
-    }
+   console.log(array)
+  }
+  const removeNames = (idx) => {
+    var array = [...names]
+    array.splice(idx, 1)
+    setNames(array)
+  }
+  const handleChange = (event) => {
+    setInput(event.target.value)
   }
   return (
     <div className="App">
       <header className="App-header">
-        {number}
-        <Button onClick={() => { changeNumber("nemeh") }}>Нэмэх</Button>
-        <Button onClick={() => { changeNumber("hasah") }}>Hasah</Button>
-        <Button onClick={() => { changeNumber("urjih") }}>Urjih</Button>
+        {input}
+        <div className='class1'>
+          <input
+          value={input}
+          onChange={(event)=> { setInput(event.target.value) }} />
+          <Button variant="outlined" onClick={addNames}>button</Button>
+        </div>
+        <ul>
+          
+          {names.map((item, idx) =>  (
+            <div className="class2">
+              <li onClick={ () => { hansad() } }>{item}</li>
+              <li onClick={ () => { removeNames(i) } }></li>
+              <li onClick={removeNames(i)}></li>
+               <button variant="outlined" onClick={() => { removeNames(idx) }}>Remove</button>
+            </div>
+          ))}
+         
+        </ul>
 
     </header>
     </div>
